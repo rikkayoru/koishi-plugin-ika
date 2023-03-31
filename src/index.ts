ec75e5520c44cebaafa3a3dcdb655875d0deb347
Derek <sakuraderek@gmail.com>
2023-03-31 11:34:44 +0800

    chore: comments

--- src/index.ts
+++ src/index.ts
@@ -14,19 +14,22 @@ export const Config: Schema<Config> = Schema.object({
   chat: Schema.object({
     enable: Schema.boolean().default(true),
     system: Schema.string()
-      .description('chat人设')
+      .description('Chat 人设')
       .default(
         '现在你是一个叫乌贼酱的16岁美少女, 性格调皮, 说话跳脱, 请根据这个设定进行对话'
       ),
     proxy: Schema.string()
       .description('Proxy addr')
       .default('socks://127.0.0.1:1080'),
-    apiKey: Schema.string().description('openAi api key').default(''),
+    apiKey: Schema.string().description('OpenAi api key').default(''),
   }),
+
   // news
   news: Schema.object({
-    enable: Schema.boolean(),
-    vendor: Schema.string().default('https://api.vvhan.com/api/60s'),
+    enable: Schema.boolean().description('启用 news 命令'),
+    vendor: Schema.string()
+      .description('新闻源, 直接返回图片的那种')
+      .default('https://api.vvhan.com/api/60s'),
   }),
 })
 
