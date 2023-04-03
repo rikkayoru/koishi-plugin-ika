c8611af7d3d435d4bc59d959fb7c321cef80f59b
Derek <sakuraderek@gmail.com>
2023-04-03 11:37:12 +0800

    fix: news not latest

--- src/index.ts
+++ src/index.ts
@@ -12,25 +12,25 @@ export * from './config'
 export const Config: Schema<Config> = Schema.object({
   // chat
   chat: Schema.object({
-    enable: Schema.boolean().default(true),
+    enable: Schema.boolean().description('启用 Chat').default(true),
     system: Schema.string()
-      .description('Chat 人设')
+      .description('人设')
       .default(
         '现在你是一个叫乌贼酱的16岁美少女, 性格调皮, 说话跳脱, 请根据这个设定进行对话'
       ),
     proxy: Schema.string()
-      .description('Proxy addr')
+      .description('代理地址')
       .default('socks://127.0.0.1:1080'),
     apiKey: Schema.string().description('OpenAi api key').default(''),
-  }),
+  }).description('Chat 设置'),
 
   // news
   news: Schema.object({
-    enable: Schema.boolean().description('启用 news 命令'),
+    enable: Schema.boolean().description('启用 news 命令').default(true),
     vendor: Schema.string()
       .description('新闻源, 直接返回图片的那种')
       .default('https://api.vvhan.com/api/60s'),
-  }),
+  }).description('每日新闻'),
 })
 
 export function apply(ctx: Context, config: Config) {
--- src/news/index.ts
+++ src/news/index.ts
@@ -1,8 +1,13 @@
+import dayjs from 'dayjs'
 import { Context } from 'koishi'
 import Config from '../config'
 
 export const applyNewsMiddleware = (ctx: Context, config: Config) => {
   ctx.command('news').action(() => {
-    return `<image url="${config.news.vendor}"></image>`
+    const today = dayjs().format('YYYY-MM-DD')
+    const vendor = new URL(config.news.vendor)
+    // prevent cache
+    vendor.searchParams.append('d', today)
+    return `<image url="${vendor.toString()}"></image>`
   })
 }
